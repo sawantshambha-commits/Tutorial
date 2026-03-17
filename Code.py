1e8a40de56ebe50c6a49f50d415646ffbdd214f1
sawantshambha-commits <sawantshambha@gmail.com>
2026-03-17 18:20:09 +0530

    Added avg functin to code.py

--- Code.py
+++ Code.py
@@ -3,3 +3,14 @@ num = int(input("Enter a number: "))
 
 for i in range(1, 11):
     print(num * i)
+
+# write python function to calculate avg of 3 user given inputs
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+avg = (a + b + c) / 3
+print("Average =", avg)
+
+
+
